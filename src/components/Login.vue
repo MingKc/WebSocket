340b7118ebbda834b33b079eeda057c14ab68972
陈柯明 <1129059298@qq.com>
2020-04-22 11:19:59 +0800

    初始化项目

---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>hello world</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        },
+        methods: {},
+        created() {}
+    }
+</script>
+
+<style lang="less" scoped>
+</style>
